d2b7297a7a843c04f78b8454f6d41c6f7a65c16c
Rosen Vitanov <rosenvitanov@gmail.com>
2024-09-10 16:26:58 +0300

    Add external input for package list to be istalled

--- depresolver.sh
+++ depresolver.sh
@@ -3,11 +3,11 @@
 ## This script is used to install the necessary packages for a script to run.
 ## Author: Rosen Vitanov
 ## Date: 2024-09-10
-## Version: 1.0
+Version="0.2"
 
 ## Here are stored all the necessary packages for the script to run"
 DEPENDENCIES=( 
-curl
+
 )
 
 ## This function checks if the necessary packages are installed.
@@ -27,8 +27,28 @@ function solve_dependencies() {
     local FLAG_SKIP_PROMPT=false
     local FLAG_YES_TO_ALL=false
 
-    while getopts "ys" opt; do
+    while getopts "hysi:" opt; do
         case $opt in
+            i) 
+                ## Input file or list of packages to check
+                ## Check if the input parameter is a file or a list of packages
+                ## If a file - check if the file exists and read the packages from it
+                ## If a list of packages - read the packages from the input
+                ## If the input is not a file or a list of packages - exit with code 1
+
+                if [ -f "$OPTARG" ]; then
+                    while IFS= read -r package; do
+                        DEPENDENCIES+=("$package")
+                    done < "$OPTARG"
+                elif [ -n "$OPTARG" ]; then
+                    IFS=',' read -ra packages <<< "$OPTARG"
+                    DEPENDENCIES+=("${packages[@]}")
+                else
+                    echo "Invalid input parameter: $OPTARG" >&2
+                    return 1
+                fi
+
+                ;;
             y)
                 ## Automatically installs missing packages without asking for user intervention, but shows the missing packages.
                 FLAG_YES_TO_ALL=true
@@ -38,6 +58,15 @@ function solve_dependencies() {
                 ## This automatically sets the -y flag
                 FLAG_SKIP_PROMPT=true
                 FLAG_YES_TO_ALL=true
+                ;;
+            h)
+                echo "depresolver.sh - Dependency resolver script"
+                echo "Version: $Version"
+                echo "Usage: $0 [-i <file|packages>] [-y] [-s]"
+                echo "Options:"
+                echo "  -i <file|packages>  Input file or list of packages to install"
+                echo "  -y                  Automatically installs missing packages without asking for user intervention, but will show the missing packages"
+                echo "  -s                  Silent automated mode - installs missing packages without asking for any user intervention or giving any ourput"
                 ;;
             \?)
                 echo "Invalid option: -$OPTARG" >&2
@@ -59,6 +88,7 @@ function solve_dependencies() {
         fi
     done
 
+    [ "$FLAG_SKIP_PROMPT" = false ] && echo ""
     if [ $missing_packages -eq 0 ]; then
         [ "$FLAG_SKIP_PROMPT" = false ] && echo "All dependencies are satisfied." 
         return 0
@@ -80,6 +110,7 @@ function solve_dependencies() {
             done
             return 0
         else
+            [ "$FLAG_SKIP_PROMPT" = false ] && echo ""
             while true; do
                 if [ "$FLAG_YES_TO_ALL" = true ]; then
                         answer="yes"
@@ -111,4 +142,4 @@ function solve_dependencies() {
     fi
 }
 
-solve_dependencies -s
+solve_dependencies
